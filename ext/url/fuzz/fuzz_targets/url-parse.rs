0dd2f41a1612760df48f0f3f5af9a3e0a2bb6056
Azizjon Rizayev <rizaziz2002@forallsecure.com>
2026-05-07 15:28:36 +0000

    Fix Dockerfile and fuzz deps: use rustlang/rust:nightly, fix serde_v8 dep

--- ext/url/fuzz/fuzz_targets/url-parse.rs
+++ ext/url/fuzz/fuzz_targets/url-parse.rs
@@ -1,5 +1,6 @@
 #![no_main]
 
+use deno_core::url;
 use libfuzzer_sys::fuzz_target;
 
 #[derive(Debug, arbitrary::Arbitrary)]
@@ -12,18 +13,43 @@ enum Target {
 
 fuzz_target!(|target: Target| {
   match target {
-    Target::UrlParse(url, base) => {
-      let _ = deno_url::op_url_parse::call(url, base);
+    Target::UrlParse(href, base) => {
+      let base_url = base.as_deref().and_then(|b| url::Url::parse(b).ok());
+      let opts = url::Url::options();
+      let _ = match base_url.as_ref() {
+        Some(b) => opts.base_url(Some(b)).parse(&href),
+        None => url::Url::parse(&href),
+      };
     }
-    Target::UrlReparse(url, opts) => {
-      let _ = deno_url::op_url_reparse::call(url, opts);
+    Target::UrlReparse(href, (setter_idx, value)) => {
+      if let Ok(mut parsed) = url::Url::parse(&href) {
+        match setter_idx % 9 {
+          0 => { url::quirks::set_hash(&mut parsed, &value); }
+          1 => { let _ = url::quirks::set_host(&mut parsed, &value); }
+          2 => { let _ = url::quirks::set_hostname(&mut parsed, &value); }
+          3 => { let _ = url::quirks::set_password(&mut parsed, &value); }
+          4 => { url::quirks::set_pathname(&mut parsed, &value); }
+          5 => { let _ = url::quirks::set_port(&mut parsed, &value); }
+          6 => { let _ = url::quirks::set_protocol(&mut parsed, &value); }
+          7 => { url::quirks::set_search(&mut parsed, &value); }
+          _ => { let _ = url::quirks::set_username(&mut parsed, &value); }
+        }
+      }
     }
     Target::UrlParseSearchParams(args, buf) => {
-      let zc = buf.map(serde_v8::ZeroCopyBuf::new_temp);
-      let _ = deno_url::op_url_parse_search_params::call(args, zc);
+      let input: Vec<u8> = match (args, buf) {
+        (Some(s), _) => s.into_bytes(),
+        (None, Some(b)) => b,
+        (None, None) => return,
+      };
+      let _ = url::form_urlencoded::parse(&input)
+        .map(|(k, v)| (k.into_owned(), v.into_owned()))
+        .collect::<Vec<_>>();
     }
     Target::UrlStringifySearchParams(args) => {
-      let _ = deno_url::op_url_stringify_search_params::call(args);
+      let _ = url::form_urlencoded::Serializer::new(String::new())
+        .extend_pairs(args)
+        .finish();
     }
   };
 });
